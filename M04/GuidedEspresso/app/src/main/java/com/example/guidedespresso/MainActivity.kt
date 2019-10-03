0b5cf18db674d611f14c77712420e93e75ab8045
Albert Yakubov <46692454+stepasha2006@users.noreply.github.com>
2019-10-03 15:07:06 -0600

    guidedProject
    successful (complete project saved in personal projects)

--- M04/GuidedEspresso/app/src/main/java/com/example/guidedespresso/MainActivity.kt
+++ M04/GuidedEspresso/app/src/main/java/com/example/guidedespresso/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.example.guidedespresso
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val KEY_TITLE = "TITLE_DATA"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initUI()
     }
+
+    private fun initUI() {
+        //add listeners to buttons
+        change_title_button.setOnClickListener {
+            title_view.text = getString()
+        }
+
+        pass_title_button.setOnClickListener {
+            val newTitle = getString()
+         //   title_view.text = newTitle
+            //passing intent to the next activity
+            val intent = Intent(this@MainActivity, ShowTitleActivity::class.java)
+            intent.putExtra(KEY_TITLE, newTitle)
+            startActivity(intent)
+        }
+    }
+
+    private fun getString() = title_input.text.toString()
+
+
 }
---
+++ M04/GuidedEspresso/app/src/main/java/com/example/guidedespresso/ShowTitleActivity.kt
@@ -0,0 +1,12 @@
+package com.example.guidedespresso
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowTitleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_title)
+    }
+}
